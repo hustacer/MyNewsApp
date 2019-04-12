44c50e2cff7220ac515a07e5e608bdd58f47ee60
hustacer <xiaobinly@gmail.com>
2019-04-12 14:58:18 +0900

    get data from xml complete

---
+++ app/src/main/java/com/example/mynewsapp/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.mynewsapp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
+    private static String URL = "http://www.imooc.com/api/teacher?type=4&num=30";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.lv_main);
+    }
+
+    private List<NewsBean> getJsonData(String url) {
+        List<NewsBean> newsBeanList = new ArrayList<>();
+        try {
+            String jsonString = readStream(new URL(url).openStream());
+            JSONObject jsonObject;
+            NewsBean newsBean;
+            try {
+                jsonObject = new JSONObject(jsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray("data");
+                for(int i = 0;i < jsonArray.length(); i++ ){
+                    jsonObject = jsonArray.getJSONObject(i);
+                    newsBean = new NewsBean();
+                    newsBean.newIconUrl = jsonObject.getString("picSmall");
+                    newsBean.newTitle = jsonObject.getString("name");
+                    newsBean.newContent = jsonObject.getString("description");
+                    newsBeanList.add(newsBean);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return newsBeanList;
+    }
+
+    private String readStream(InputStream is) {
+        InputStreamReader isr;
+        String result = "";
+        try {
+            String line = "";
+            isr = new InputStreamReader(is, "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+
+            isr.close();
+            br.close();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    class NewsAsyncTask extends AsyncTask<String, Void, List<NewsBean>> {
+        @Override
+        protected List<NewsBean> doInBackground(String... params) {
+            // get data from xml
+            return getJsonData(params[0]);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mynewsapp/NewsBean.java
@@ -0,0 +1,7 @@
+package com.example.mynewsapp;
+
+public class NewsBean {
+    public String newIconUrl;
+    public String newTitle;
+    public String newContent;
+}
